dcc46cd213c622280673f9b233816857a9ff248c
Hiskel Dibera <hiskeldibera@gmail.com>
2026-07-14 16:02:28 +0300

    Added registration feature

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js"
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Chat server is running...");
+});
+
+app.use("/api/auth", authRoutes);
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
